ad3968836574073ffea4e20165c6eb9d3dd85714
Elstan <vzmraven@mail.ru>
2020-08-11 16:27:47 +0300

    my_balls blank +

--- my_balls.py
+++ my_balls.py
@@ -4,11 +4,14 @@ from random import randrange as rnd, choice
 root = Tk()
 root.geometry("800x600")
 
-canv = Canvas(root, width=500, height=500, bg='white')
+canv = Canvas(root, bg='lightblue')
 canv.pack(fill=BOTH, expand=1)
 
 colors = ['red', 'blue', 'green', 'orange', 'yellow', 'black']
+
+
 def new_ball():
+    """Функция отображения шарика случайного размера в случайном месте"""
     global x, y, R
     canv.delete(ALL)
     x = rnd(100, 700)
@@ -17,8 +20,11 @@ def new_ball():
     canv.create_oval(x - R, y - R, x + R, y + R, fill=choice(colors), width=0)
     root.after(1000, new_ball)
 
+
 def click(event):
+    """Функциия регистрации клика лефой кнопкой мыши"""
     print(x, y, R)
+
 
 new_ball()
 canv.bind('<Button-1>', click)
